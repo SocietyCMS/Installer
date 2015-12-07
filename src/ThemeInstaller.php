1208df6da7ea566a56820452bf91eebf9a356969
Ralph Huwiler <ralph@huwiler.rocks>
2015-12-07 09:41:31 +0100

    Add ThemeInstaller

---
+++ src/ThemeInstaller.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace SocietyCMS\Composer;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+
+class ThemeInstaller extends LibraryInstaller
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package)
+    {
+        $prefix = substr($package->getPrettyName(), 0, 17);
+        if ('societycms/theme-' !== $prefix) {
+            throw new \InvalidArgumentException(
+                'Unable to install theme, societycms themes '
+                .'should always start their package name with '
+                .'"societycms/theme-"'
+            );
+        }
+
+        return 'themes/'.substr($package->getPrettyName(), 17);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType)
+    {
+        return 'societycms/theme' === $packageType;
+    }
+}
---
+++ src/ThemeInstallerPlugin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SocietyCMS\Composer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class ThemeInstallerPlugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $installer = new ThemeInstaller($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
+}
